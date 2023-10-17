38c2366e205dca2ed2d92c2fb8f564d3999697c2
vignesh <vigneshfornavy@gmail.com>
2023-10-17 21:56:09 +0530

    Last File

--- app.js
+++ app.js
@@ -1,40 +1,31 @@
-let Colors = ["yellow","green","red","purple"];
-let h5 = document.querySelector("h5");
-let started = false;
+let gameOrder = [];
+let userGameOrder = [];
+let btns = ["yellow","red","green","purple"];
 let level = 0;
+let started = false;
+
+let h5 = document.querySelector("h5");
 document.addEventListener("keypress",()=>{
     if(started == false){
-        console.log("game is started");
         started = true;
-        LevelUp();
+        levelUp();
     }
-})
+});
 
-function LevelUp(){
-    level++;
-    h5.innerText = `Level ${level}`;
-    let randomColor = Math.floor(Math.random()*3);
-    let color = Colors[randomColor];
-    let rand = document.querySelector(`.${color}`);
-    console.log(randomColor);
-
-    BtnFLash(rand);
-}
-
-function BtnFLash(btn){
+function gameFlash(btn){
     btn.classList.add("flash");
     setTimeout(function(){
         btn.classList.remove("flash");
-    },500)
+    },1000)
 }
 
+function levelUp(){
+    level++;
+    h5.innerText = `level ${level}`;
 
-function BtnPress(){
-    console.log("clicked");
-};
+    let indexOfButtons = Math.floor(Math.random()*3);
+    let color = btns[indexOfButtons];
+    let colorSel = document.querySelector(`.${color}`);
+    gameFlash(colorSel);
 
-let fourButtons = document.querySelectorAll(".box");
-
-for(fourBtns of fourButtons){
-    fourBtns.addEventListener("click",BtnPress);
 }
